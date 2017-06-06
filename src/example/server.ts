578517580a93a01ae714f0720a600f57c94e4966
qmShen <joyshen06@gmail.com>
2017-06-06 19:39:00 +0800

    init socket

--- src/example/server.ts
+++ src/example/server.ts
@@ -1,14 +1,13 @@
-import * as http from 'http';
 import monitor from '../index'
 import * as koa from "koa"
 import * as Router from 'koa-router'
 const app = new koa()
 
-app.use(monitor(app))
+app.use(monitor(app, null))
 let router = new Router()
     .get('/', async (ctx, next) => {
         ctx.body = 'Hello World4'
     })
     
 app.use(router.routes())
-app.listen(3001)
+app.listen(3000)
--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,41 @@
-'use strict'
-import * as pidusage from "pidusage"
-import monitoringMiddlewareWrapper from '../dist/src/index';
+"use strict";
+import * as pidusage from "pidusage";
+import * as socketIo from "socket.io";
+
 
 export let GLOBALCONFIG = {
     timeout: 2,
     monitorLength: 20,
     spans: [{
         responses: [],
-        timeRange: 1
+        timeRange: 1,
     }, {
         responses: [],
-        timeRange: 5
+        timeRange: 5,
     }, {
         responses: [],
-        timeRange: 10
-    }]
-}
+        timeRange: 10,
+    }],
+};
 
+let io = null;
 
 /**
  * @param [Object] statOption
- *   - {number} timestampe: timestamp to generate this status record  
+ *   - {number} timestampe: timestamp to generate this status record
  *   - {number} cpu: the usage of cpu
  *   - {number} memory: the usage of memory
  *   - {number} count: the number of response during the record range
  *   - {number} responseTime: average response time during the record range(timeRange)
  *   - {number} timeRange: the time range of this record
  */
-export interface statOption {
-    timestamp?: number,
-    cpu?: number,
-    memory?: number,
-    count?: number,
-    responseTime?: number,
-    timeRange?: number
+export interface StatOption {
+    timestamp?: number;
+    cpu?: number;
+    memory?: number;
+    count?: number;
+    responseTime?: number;
+    timeRange?: number;
 }
 
 // Return the last element of an array, return null if bad input
@@ -44,61 +46,78 @@ function lastElement(array) {
 function collectUsage(span) {
     pidusage.stat(process.pid, (err, stat) => {
         // remove the first element of the response list if the length longer than monitorLength
-        if (span.responses.length >= GLOBALCONFIG.monitorLength / span.timeRange) span.responses.shift();
-
-        // Collect the memory, cpu, timestamp; 
-        // Init the response count, response time and timerange  
-        const statRecord: statOption = {
-            memory: stat.memory / 1024 / 1024, //Unit: M
+        if (span.responses.length >= GLOBALCONFIG.monitorLength / span.timeRange) {
+            span.responses.shift();
+        }
+        // Collect the memory, cpu, timestamp;
+        // Init the response count, response time and timerange
+        const statRecord: StatOption = {
+            memory: stat.memory / 1024 / 1024, // Unit: M
             cpu: stat.cpu,
             timestamp: Date.now(),
             count: 0,
             responseTime: 0,
-            timeRange: span.timeRange
+            timeRange: span.timeRange,
         };
-        span.responses.push(statRecord)
-    })
+        span.responses.push(statRecord);
+        emitStat(span);
+    });
 }
 
 function responseCount(lastResponses) {
-    lastResponses.forEach(lastResponse => {
-        if (!lastResponse) return;
+    lastResponses.forEach((lastResponse) => {
+        if (!lastResponse) {return; }
         lastResponse.count++;
-        let meanTime: number = lastResponse.responseTime;
+        const meanTime: number = lastResponse.responseTime;
         lastResponse.responseTime = meanTime + (GLOBALCONFIG.timeout * 1000 - meanTime) / lastResponse.count;
-    })
-
+    });
 }
 
 function responseTime(startTime, lastResponses) {
-    lastResponses.forEach(lastResponse => {
-        if (!lastResponse) return;
+    lastResponses.forEach((lastResponse) => {
+        if (!lastResponse) {return; }
         let responseTime: number = process.hrtime(startTime);
         responseTime = responseTime[0] * 10e3 + responseTime[1] * 10E-6;
-        let meanTime: number = lastResponse.responseTime;
+        const meanTime: number = lastResponse.responseTime;
         lastResponse.responseTime = meanTime + (responseTime - GLOBALCONFIG.timeout * 1000) / lastResponse.count;
-    })
+    });
 }
 
 function startMonitoring() {
     GLOBALCONFIG.spans.forEach((span) => {
-        const interval: any = setInterval(() => collectUsage(span), span.timeRange * 1000);
-        interval.unref()
-    })
-
+        const interval: any = setInterval(() => {
+            collectUsage(span);
+        }, span.timeRange * 1000);
+        interval.unref();
+    });
 }
 
+function emitStat(span) {
+    io.emit("stats", {
+      responses: span.responses[span.responses.length - 2],
+      timeRange: span.timeRange,
+    });
+} 
+
 export default function monitoringMiddlewareWrapper(app, config) {
+    io = socketIo(app)
+    io.on('connection',(socket)=>{
+        console.log('connected');
+    })
     startMonitoring();
     return async function monitoring(ctx, next) {
         const startTime = process.hrtime();
-        let lastResponses = [];
-        GLOBALCONFIG.spans.forEach(function (span) {
+        const lastResponses = [];
+        GLOBALCONFIG.spans.forEach((span) => {
             lastResponses.push(lastElement(span.responses));
-        })
+        });
         responseCount(lastResponses);
-        console.log(GLOBALCONFIG.spans[0])
         await next();
         responseTime(startTime, lastResponses);
-    }
-}
+    };
+};
+
+
+
+
+
