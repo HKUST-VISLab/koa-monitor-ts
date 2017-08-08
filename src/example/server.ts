23e57f7a3286dc934836ac3e5e18a1471a3ebbc5
qmShen <joyshen06@gmail.com>
2017-08-08 20:22:40 +0800

    refactor

--- src/example/server.ts
+++ src/example/server.ts
@@ -7,7 +7,7 @@ const app = new koa()
 app.use(monitor(app, {path:'/status'}))
 let router = new Router()
     .get('/', async (ctx, next) => {
-        ctx.body = 'Hello World'
+        ctx.body = 'Hello World2'
     })
     
 app.use(router.routes())
